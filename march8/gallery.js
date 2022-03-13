f253d8470adffc62ef13cda96a1084ca08c532c8
kritikapat <96338775+kritikapat@users.noreply.github.com>
2022-03-13 18:36:20 +0530

    Add files via upload

---
+++ march8/gallery.js
@@ -0,0 +1,19 @@
+
+function x(n){
+
+    var a = document.getElementsByClassName("con")[0];
+    
+    
+    // a.classList.remove("main");
+   
+   
+    // var b = document.getElementsByClassName("zoom")[0];
+    var b = document.getElementsByClassName("dis")[n];
+    
+        a.style.display = "none";
+    
+    b.style.display = "initial";
+      
+
+   }
+
